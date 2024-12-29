eaa9880acc3c4c1e25493ce6a63e068a184414dd
abdullah <abdullahahmed1991998@gmail.com>
2024-12-29 09:45:37 +0300

    git add all

---
+++ lib/core/routing/app_router.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_advanced_course/core/routing/routes.dart';
+import 'package:flutter_advanced_course/feature/login/presentation/pages/login_page.dart';
+import 'package:flutter_advanced_course/feature/onboarding/presentation/pages/onboarding_page.dart';
+
+class AppRouter{
+
+
+
+   Route onGenerateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.onBoardingPage:
+        return MaterialPageRoute(builder: (_) => const OnBoardingPage());
+      case Routes.loginPage:
+        return MaterialPageRoute(builder: (_) => const LoginPage());
+      default:
+        return MaterialPageRoute(builder: (_) => const Scaffold(
+          body: Center(child: Text('Page not found'),),
+        ));
+    }
+  }
+}
---
+++ lib/core/routing/routes.dart
@@ -0,0 +1,5 @@
+class Routes {
+  static const String onBoardingPage = 'onBoardingPage';
+  static const String loginPage = 'loginPage';
+  static const String homePage = 'homePage';
+}
